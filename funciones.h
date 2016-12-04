9a8c9a25403f799a3c3aa7d77fa538b016e7fa7d
Daniel Ferrer Pérez <daniel10_es@yahoo.es>
2016-12-04 18:06:55 +0100

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,87 @@
+void floatToString (float real) {
+  int ent;
+  int dec;
+  char buf[12];
+  String enteroTexto;   
+  String decimalTexto;  
+  ent = int(floor(real));
+  if ((round(real*10) - floor(real)*10) == 10) {
+    dec = 0;
+  } else {
+    dec = round(real*10) - floor(real)*10;
+  }
+    enteroTexto=itoa(ent,buf,10);
+    decimalTexto=itoa(dec,buf,10); 
+    realTexto = enteroTexto  + "." + decimalTexto; 
+}
+
+void filtro(int temperatura, int humedad){
+  static float hume[10];
+  static float temp[10];
+  static int x = 0;
+  static int y = 0;
+  static int humedad_max_i = 0;
+  static int humedad_min_i = 0;
+  static float humedad_max = 0.0;
+  static float humedad_min = 0.0;
+  static float humedad_suma = 0.0;
+  static int temperatura_max_i = 0;
+  static int temperatura_min_i = 0;
+  static float temperatura_max = 0.0;
+  static float temperatura_min = 0.0;
+  static float temperatura_suma = 0.0;
+  
+  if ((humedad >= 0.0) && (humedad <=100.0) && (temperatura >= -20.0) && (temperatura <= 60.0)) {
+  temperatura = temperatura +  (0.0);
+  humedad = humedad + (0.0);
+  /*Calculo de la humedad filtrada*/
+  if (x < 10) { hume[x] = humedad; temp[x] = temperatura; x++; } 
+  if (x >= 10) { x=0; }
+
+  humedad_max = 0.0;
+  humedad_min = 100.0;
+  temperatura_max = -30.0;
+  temperatura_min = 100.0;
+  for(int q=0;q<10;q++) {
+    if (hume[q] > humedad_max) {
+      humedad_max = hume[q];
+      humedad_max_i = q;
+    }
+    if (hume[q] < humedad_min) {
+      humedad_min = hume[q];
+      humedad_min_i = q;
+    }
+    if (temp[q] > temperatura_max) {
+      temperatura_max = temp[q];
+      temperatura_max_i = q;
+    }
+    if (temp[q] < temperatura_min) {
+      temperatura_min = temp[q];
+      temperatura_min_i = q;
+    }
+  }
+  humedad_suma = 0.0;
+  temperatura_suma = 0.0;
+  for(int q=0;q<10;q++) { 
+    if (humedad_max_i == humedad_min_i) {if (humedad_max_i < 8) {humedad_max_i++;} else {humedad_max_i--;}}
+    if ((q != humedad_max_i) && (q != humedad_min_i)) {
+      humedad_suma = humedad_suma + hume[q];
+    }
+    if (temperatura_max_i == temperatura_min_i) {if (temperatura_max_i < 8) {temperatura_max_i++;} else {temperatura_max_i--;} }
+    if ((q != temperatura_max_i)&&(q != temperatura_min_i)) {
+      temperatura_suma = temperatura_suma + temp[q];
+    }
+  }
+  humedad_filtrada = humedad_suma/8;
+  temperatura_filtrada = temperatura_suma/8;
+
+  humedad_parte_entera = round(humedad_filtrada);
+  temperatura_parte_entera = floor(temperatura_filtrada);
+  if ((round(temperatura_filtrada*10) - floor(temperatura_filtrada)*10) == 10) {
+    temperatura_parte_decimal = 0;
+  } else {
+    temperatura_parte_decimal = round(temperatura_filtrada*10) - floor(temperatura_filtrada)*10;
+  }
+  }
+}
+
---
+++ variables.h
@@ -0,0 +1,49 @@
+//--------------DEFINE-------------------------------------
+#define DHT22_PIN         D3
+#define ENCODER_A_PIN     D7
+#define ENCODER_B_PIN     D6
+#define ENCODER_PUL_PIN   D5
+#define OLED_RESET         0 
+
+//--------------VARIABLES-------------------------------------
+const unsigned long MAXUL = 4294967295UL; 
+unsigned long LT[10] = {0,0,0,0,0,0,0,0,0,0};
+unsigned long MS[10] = {100,200,500,1000,2000,5000,10000,30000,60000,300000};
+
+/* DHT22 */
+bool  inestable;
+float humedad;
+float temperatura;
+float setpoint;
+float setpoint_anterior;
+float humedad_filtrada;
+float temperatura_filtrada;
+int   humedad_parte_entera;
+int   temperatura_parte_entera;
+int   temperatura_parte_decimal;
+
+/* Encoder */
+volatile int  encoder_valor_actual;
+volatile bool encoder_evento_giro;
+volatile bool encoder_evento_pulsador;
+volatile bool encoder_estado_pulsador;
+bool          encoder_aux1;
+bool          encoder_aux2;
+int           aux;
+
+/* ThingSpeak */
+unsigned long myChannelNumber = xxx;
+const char * myWriteAPIKey = "xxx";
+String    thingtweetAPIKey = "xxx";
+char   thingSpeakAddress[] = "api.thingspeak.com";
+long    lastConnectionTime = 0; 
+boolean      lastConnected = false;
+int          failedCounter = 0;
+
+/* Para pasar de real a cadena */
+byte aa;
+String realTexto;       /* Texto entero + decimal */
+char buf[12];
+
+
+
